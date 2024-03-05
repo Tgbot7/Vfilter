8698eea736b7bbfd8040dcb6c0fb3029dadd4b85
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2024-03-05 22:10:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/test1.git /LazyPrincess
+  git clone https://github.com/skcreator7/filterbot.git /filterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /filterbot
 fi
-cd /LazyPrincess
+cd /filterbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
