d09f31556f5efe122b9c422606b29518dc8765a5
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2024-03-06 07:21:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -185,7 +185,9 @@ async def doc(bot, update):
         await ms.delete() 
         os.remove(file_path) 
         if ph_path:
-           os.remove(ph_path)
+           os.remove(ph_path) 
+    except Exception as e:
+        logger.error(f"error 2 : {e}")
 
 # Born to make history @LazyDeveloper !
 @Client.on_callback_query(filters.regex(r"^next"))
